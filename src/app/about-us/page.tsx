04785f57c6f9438d3ad80dd19438255d2a752a72
Sachin sharma <sachin.sharma.6091@gmail.com>
2024-05-30 16:15:59 +0530

    resolve build error

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,12 +1,5 @@
-import FeedbackCarousel from '@/components/FeedbackCarousel';
-
 const AboutUs = () => {
-    return (
-        <div className="text-center font-bold py-10">
-            Work In Progress
-            <FeedbackCarousel heading="sachin" leftScrollValue={400} rightScrollValue={400} />
-        </div>
-    );
+    return <div className="text-center font-bold py-10">Work In Progress</div>;
 };
 
 export default AboutUs;
--- src/hooks/useScreen.ts
+++ src/hooks/useScreen.ts
@@ -1,22 +1,29 @@
 import { useState, useEffect } from 'react';
 
 const useScreenSize = () => {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [windowWidth, setWindowWidth] = useState(
+        typeof window !== 'undefined' ? window.innerWidth : 0,
+    );
 
     const handleResize = () => {
         setWindowWidth(window.innerWidth);
     };
 
     useEffect(() => {
-        window.addEventListener('resize', handleResize);
+        if (typeof window !== 'undefined') {
+            window.addEventListener('resize', handleResize);
 
-        // Cleanup function to remove event listener on unmount
-        return () => window.removeEventListener('resize', handleResize);
+            // Initial call to set the window width
+            handleResize();
+
+            // Cleanup function to remove event listener on unmount
+            return () => window.removeEventListener('resize', handleResize);
+        }
     }, []);
 
     // Define breakpoints for different screen sizes (customize these values)
     const isMobile = windowWidth <= 768;
-    const isTablet = windowWidth <= 1024;
+    const isTablet = windowWidth > 768 && windowWidth <= 1024;
     const isDesktop = windowWidth > 1024;
 
     return { isMobile, isTablet, isDesktop };
