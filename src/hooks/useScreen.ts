58d5e3ad913dda15792e31823bed97e9550dbf30
Sachin sharma <sachin.sharma.6091@gmail.com>
2024-05-30 17:01:40 +0530

    resolve responsive bugs

--- src/hooks/useScreen.ts
+++ src/hooks/useScreen.ts
@@ -1,32 +1,13 @@
-import { useState, useEffect } from 'react';
+import useWindowDimensions from '@/hooks/useWindowDimensions';
 
 const useScreenSize = () => {
-    const [windowWidth, setWindowWidth] = useState(
-        typeof window !== 'undefined' ? window.innerWidth : 0,
-    );
+    const dimension: any = useWindowDimensions();
 
-    const handleResize = () => {
-        setWindowWidth(window.innerWidth);
+    return {
+        isMobile: dimension.width <= 768,
+        isTablet: dimension.width <= 1024,
+        isDesktop: dimension.width > 1024,
     };
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            window.addEventListener('resize', handleResize);
-
-            // Initial call to set the window width
-            handleResize();
-
-            // Cleanup function to remove event listener on unmount
-            return () => window.removeEventListener('resize', handleResize);
-        }
-    }, []);
-
-    // Define breakpoints for different screen sizes (customize these values)
-    const isMobile = windowWidth <= 768;
-    const isTablet = windowWidth > 768 && windowWidth <= 1024;
-    const isDesktop = windowWidth > 1024;
-
-    return { isMobile, isTablet, isDesktop };
 };
 
 export default useScreenSize;
