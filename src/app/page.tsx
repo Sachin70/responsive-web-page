4ed988ac4c7156443e46172b2ca40d4a9e32c30c
Sachin sharma <sachin.sharma.6091@gmail.com>
2024-05-30 15:57:33 +0530

    removed commented code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,6 @@ import Careers from '@/components/Careers';
 import VideoSection from '@/components/VideoSection';
 import FeedbackCarousel from '@/components/FeedbackCarousel';
 import OlveraGuides from '@/components/OlveraGuides';
-
-import { companyData } from '@/utils/constants';
 import CompanyData from '@/components/CompanyDataSection';
 
 export default function Home() {
@@ -16,17 +14,6 @@ export default function Home() {
         <>
             <HeroSection />
             <AboutOlvera />
-            {/* <div className="flex flex-wrap justify-between gap-5 px-16 py-7 md:py-12 md:px-[140px]">
-                {companyData.map((data) => (
-                    <div key={data?.id} className="flex flex-col gap-1 md:gap-7">
-                        <p className="text-primary font-extrabold text-5xl leading-76">
-                            {data?.value}
-                        </p>
-
-                        <p className="text-black1 font-semibold text-xl leading-6">{data?.label}</p>
-                    </div>
-                ))}
-            </div> */}
             <CompanyData />
             <Services />
             <OlveraGuides />
